00b538ae2c852122c4e2ca8896444ada69d68cec
nicolegobo <nbowers@mich.edu>
2023-04-27 15:03:09 -0500

    removing summary table .HTML, adding multicomparison.CSV

--- workflow/scripts/pavian_multi_sample_comparsion.R
+++ workflow/scripts/pavian_multi_sample_comparsion.R
@@ -14,6 +14,13 @@ xargs<- parser$parse_args()
 ### Get sample names from input files to name columns of report ###
 input_list <- unlist(strsplit(xargs$input, " "))
 
+# # if only one sample is run, the multicomparison cannot be preformed 
+# if (length(my_list) == 1) {
+#   print("one item in list")
+# } else {
+#   print("MORE than one item in list")
+# }
+
 # Initialize an empty list
 sample_name_list <- list()
 input_file_list <- list()
@@ -37,14 +44,15 @@ reports <- pavian::read_reports(input_file_vector)
 ### Prep strings for output files
 out_file_path <- xargs$output
 multisample_comparison_html <- paste0(out_file_path, 'multisample_comparison.html')
-summary_table_html <- paste0(out_file_path, 'summary_table.html')
-summary_table_csv <- paste0(out_file_path, 'summary_table.CSV')
-
+multi_sample_table_csv <- paste0(out_file_path, 'multi_sample_table.csv')
+summary_table_csv <- paste0(out_file_path, 'summary_table.csv')
 
 ### this is where the analysis begins
 ### from pavain https://github.com/fbreitwieser/pavian
 merged_reports <- pavian::merge_reports2(reports, col_names = sample_name_list)
 summary_report <- pavian::summarize_reports(reports)
+### export report summary as .CSV ###
+write.csv(summary_report, summary_table_csv, row.names=TRUE)
 
 tax_data <- merged_reports[[1]]
 clade_reads <- merged_reports[[2]]
@@ -63,6 +71,7 @@ tax_data1 <- tax_data[sel_rows,]
 filtered_clade_reads1 <- filtered_clade_reads[sel_rows, ]
 taxon_reads1 <- taxon_reads[sel_rows, ]
 
+# where we run into a problem 
 head(cbind(tax_data1[,1:3],clade_reads[sel_rows, ])[order(-apply(filtered_clade_reads1,1,max, na.rm=T)),])
 normalized_clade_reads <- normalize(filtered_clade_reads1)
 normalized_taxon_reads <- normalize(taxon_reads[sel_rows,], sums = colSums(filtered_clade_reads1,na.rm = T))
@@ -95,9 +104,5 @@ multi_sample_table <- DT::datatable(reads_zscore_df,
 ### make output files ###
 # export multisample_compairision as .HTML
 htmlwidgets::saveWidget(multi_sample_table, multisample_comparison_html )
-# export report summary as .HTML
-summary_report_dt <- DT::datatable(summary_report)
-htmlwidgets::saveWidget(summary_report_dt, file = summary_table_html)
-
-### export report summary as .CSV ###
-write.csv(summary_report, summary_table_csv, row.names=TRUE)
+# export as CSV for potential further visualization 
+write.csv(reads_zscore_df, multi_sample_table_csv)
